fe1b6e73daba24d673a9a6e3e03cc6c39c3b9794
Bruno Calhau <bruno_calhau@hotmail.com>
2021-02-20 09:57:36 -0300

    Commit inicial, funcionalidade ok

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  items:any;
 
+  constructor() {
+    this.items = [
+      {title: 'Slide item1'},
+      {title: 'Slide item2'},
+      {title: 'Slide item3'},
+      {title: 'Slide item4'},
+      {title: 'Slide item5'},
+      {title: 'Slide item6'}
+  ];
+  }
+
+  removeItem(item){
+    // evento.detail.ratio = 0
+    for(let i = 0; i < this.items.length; i++) {
+            if(this.items[i] == item){
+        this.items.splice(i, 1);
+      }
+    }
+  }
+
+  teste(evento: CustomEvent){
+    console.log('Oi Bruno, segue o evento: ', evento)
+    console.log('Ratio', evento.detail.ratio)
+
+  }
+
+  teste2(evento){
+    console.warn('Entrei no swipe')
+    evento.detail.ratio = 0
+  }
 }
